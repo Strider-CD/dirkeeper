a645fa0bce459a2c28e5b35e41002a6c101aef5e
Niall O'Higgins <niallo@frozenridge.co>
2013-10-29 16:46:15 -0700

    tests passing so far.

---
+++ index.js
@@ -0,0 +1,74 @@
+var async  = require('async')
+var fs     = require('fs')
+var mkdir  = require('mkdirp')
+var path   = require('path')
+var rmdir  = require('rimraf')
+
+module.exports = function(opts, cb) {
+
+    var baseDir = opts.baseDir
+
+    var pruneToCount = function(count, dirs, cb) {
+      // count of 0 means prune everything
+      if (count === 0) {
+        count = dirs.length
+      }
+
+      // sort by mtime, oldest first
+      dirs = dirs.sort(function(a, b) {
+        return a.stats.mtime.getTime() > b.stats.mtime.getTime()
+      })
+
+      // trim to *count* entries
+      dirs = dirs.slice(0, count)
+
+      // delete *count* oldest directories
+      var rmFuncs = []
+      dirs.forEach(function(d) {
+        rmFuncs.push(function(cb) {
+          var fname = path.join(baseDir, d.name)
+          rmdir(fname, cb)
+        })
+      })
+
+      // rmdir in parallel
+      async.parallel(rmFuncs, cb)
+    }
+
+    var baseDirResults = function(err, files) {
+      if (err) {
+        // Ignore it if it doesn't exist
+        if (err.code === 'ENOENT') {
+          return cb(null, null)
+        }
+        console.log(err)
+        return cb(err)
+      }
+
+      if (files.length === 0) {
+        return cb(null, null)
+      }
+
+      var statFuncs = []
+      files.forEach(function(f) {
+        statFuncs.push(function(cb) {
+          fs.stat(path.join(opts.baseDir, f), function(err, res) {
+            // Might want to ignore errors?
+            if (err) return cb(err)
+            // Ignore non-directories
+            if (res.isDirectory()) {
+              return cb(null, {name:f, stats: res})
+            }
+            return cb(null, null)
+          })
+        })
+      })
+
+      async.parallel(statFuncs, function(err, dirs) {
+        if (err) return cb(err)
+        pruneToCount(opts.count, dirs, cb)
+      })
+    }
+
+    fs.readdir(opts.baseDir, baseDirResults)
+}
---
+++ test/test.js
@@ -0,0 +1,99 @@
+var async  = require('async')
+var expect = require('chai').expect
+var fs     = require('fs')
+var keeper = require('../index.js')
+var mkdirp = require('mkdirp')
+var path   = require('path')
+var rmdir  = require('rimraf')
+
+
+var EMPTY_DIR = "/tmp/dirkeeper.test.empty"
+var FOUR_SUBDIRS = "/tmp/dirkeeper.test.four"
+var TEN_SUBDIRS = "/tmp/dirkeeper.test.ten"
+
+function makeDirs(baseDir, numDirs, cb) {
+  var count = 0
+
+  async.whilst(
+    function() { return count < numDirs },
+    function(cb) {
+      mkdirp(path.join(baseDir, "tmpfile." + count), function(err) {
+        count++
+        // Wait 1 second between files
+        setTimeout(function() {
+            cb(err)
+        }, 1000)
+      })
+    },
+    cb
+  )
+}
+
+describe('#dirkeeper', function() {
+
+  before(function(done) {
+    this.timeout(20000)
+    async.parallel([
+      function(cb) {
+        mkdirp(EMPTY_DIR, cb)
+      },
+      function(cb) {
+        mkdirp(FOUR_SUBDIRS, function(err) {
+          if (err) return cb(err)
+          makeDirs(FOUR_SUBDIRS, 4, cb)
+        })
+      },
+      function(cb) {
+        mkdirp(TEN_SUBDIRS, function(err) {
+          if (err) return cb(err)
+          makeDirs(TEN_SUBDIRS, 10, cb)
+        })
+      }
+    ], done)
+  })
+
+  after(function(done) {
+    async.parallel([
+      function(cb) {
+        rmdir(EMPTY_DIR, cb)
+      },
+      function(cb) {
+        rmdir(FOUR_SUBDIRS, cb)
+      },
+      function(cb) {
+        rmdir(TEN_SUBDIRS, cb)
+      }
+    ], done)
+  })
+
+  it('should succeed on non-existant directories', function(done) {
+    var opts = {
+      baseDir: "/this/path/doesnt/exist/i/am/sure/of/it" + Math.floor(Math.random() * 10000)
+    }
+
+    keeper(opts, function(err) {
+      expect(err).to.be.null
+      done()
+    })
+  })
+
+  it('should succeed on empty directories', function(done) {
+    keeper({ baseDir: EMPTY_DIR }, function(err) {
+      expect(err).to.be.null
+      done()
+    })
+  })
+
+  it('should delete all sub-directories if opts.count is 0', function(done) {
+
+    keeper({ baseDir: FOUR_SUBDIRS, count: 0 }, function(err) {
+
+      fs.readdir(FOUR_SUBDIRS, function(err, files) {
+        expect(files).to.have.length(0)
+        done()
+      })
+
+    })
+  })
+
+})
